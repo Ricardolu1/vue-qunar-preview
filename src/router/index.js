1fa5d186026cb340004e9643c4b6fd999703a6b6
Ricardo <568082704@qq.com>
2020-01-20 01:11:05 +0800

    save

--- src/router/index.js
+++ src/router/index.js
@@ -12,23 +12,17 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: {
-        main:Home
-      }
+      component:Home
     },
     {
       path: '/city',
       name: 'City',
-      component:{
-        main:City
-      }
+      component:City
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: {
-        main:"Detail"
-      }
+      component:Detail
     }
   ],
   scrollBehavior (to, from, savedPosition) {
